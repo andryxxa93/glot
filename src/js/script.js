1059bfb4cff7d1d6c313e5c3e59f93b42415e102
AndrewHome <levanevskogo79@gmail.com>
2020-03-22 13:39:51 +0300

    add slick slider

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.feed__slider').slick(
+        {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            centerMode: true,
+          }
+    );
+  });
