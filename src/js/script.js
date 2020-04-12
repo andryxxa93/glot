21283722bfb3d321f1475b677796e4e64698e638
AndrewHome <levanevskogo79@gmail.com>
2020-04-12 18:08:56 +0300

    adaptation 767px

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,26 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.png"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png"></img></button>'
       });
+
+    const hamburger = document.querySelector('.hamburger'),
+        overlay = document.querySelector('.overlay'),
+        nav = document.querySelector('nav'),
+        menu = nav.querySelector('.header__menu'),
+        button = menu.querySelector('.button'),
+        menuItems = menu.querySelectorAll('.header__item');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header_active');
+        button.classList.toggle('button_hidden');
+    });
+
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header_active');
+            button.classList.toggle('button_hidden');
+        });
+    });
+
   });
